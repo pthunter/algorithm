425598d5526a90108db6c3dc08434d131f57ad95
pthunter <pthunter@users.noreply.github.com>
2017-10-29 23:15:35 -0400

    stockIV

--- stock/188. Best Time to Buy and Sell Stock IV.java
+++ stock/188. Best Time to Buy and Sell Stock IV.java
@@ -1,20 +1,55 @@
 class Solution {
+
+    // better?  // everyday can do one buy + one sell ?
+
+    public int maxProfit(int k, int[] prices) {
+        int len = prices.length;
+        if (k >= len / 2) return quickSolve(prices);
+
+        int[][] t = new int[k + 1][len];
+        for (int i = 1; i <= k; i++) {
+            int tmpMax =  -prices[0];
+            for (int j = 1; j < len; j++) {
+                t[i][j] = Math.max(t[i][j - 1], prices[j] + tmpMax);
+                tmpMax =  Math.max(tmpMax, t[i - 1][j - 1] - prices[j]);
+            }
+        }
+        return t[k][len - 1];
+    }
+
+
+    private int quickSolve(int[] prices) {
+        int len = prices.length, profit = 0;
+        for (int i = 1; i < len; i++)
+            // as long as there is a price gap, we gain a profit.
+            if (prices[i] > prices[i - 1]) profit += prices[i] - prices[i - 1];
+        return profit;
+    }
+
+
+
+
+
+
+
+
+
     //hold[i][k]  ith day k transaction have stock and maximum profit
     //unhold[i][k] ith day k transaction do not have stock at hand and maximum profit
-    public int maxProfit(int k, int[] prices) {
+    public int maxProfit_not_easy_to_understand(int k, int[] prices) {
         if(k>prices.length/2) return maxP(prices);
         int[][] hold = new int[prices.length][k+1];
         int[][] unhold = new int[prices.length][k+1];
-        
-        hold[0][0] = -prices[0];                       // why?  MinValue?  
+
+        hold[0][0] = -prices[0];                       // why?  MinValue?
             //->  k(one buy one sell , k=1)   hold start from 0, unhold start from 1 ??????????????????????
         //hold[0][0] = Integer.MIN_VALUE;                       // why?  MinValue?
-        
+
         for(int i=1;i<prices.length;i++) hold[i][0] = Math.max(hold[i-1][0],-prices[i]);
         //for(int i=1;i<prices.length;i++) hold[i][0] = Integer.MIN_VALUE;
 
         for(int j=1;j<=k;j++) hold[0][j] = -prices[0];  // here make sense
-        
+
         for(int i=1;i<prices.length;i++){
             for(int j=1;j<=k;j++){
                 hold[i][j] = Math.max(unhold[i-1][j]-prices[i],hold[i-1][j]);        // unhold start from 0
